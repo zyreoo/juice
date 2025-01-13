aa166605c784c20129f729b8f0085b1a92f3c3e4
SerenityUX <thomas@serenidad.app>
2025-01-13 14:34:02 -0500

    hello world

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cafe Orpheus - Shanghai</title>
+        <meta name="description" content="cafe" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+
+      </div>
+    </>
+  );
+}
