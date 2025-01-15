362c4e008d936b6e8de184239704b7615304fff3
SerenityUX <thomas@serenidad.app>
2025-01-14 21:27:54 -0500

    hello world

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
-import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,18 +16,41 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+function Cube() {
+  const meshRef = useRef();
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta * 0.5;
+    meshRef.current.rotation.y += delta * 0.5;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Cafe Orpheus - Shanghai</title>
-        <meta name="description" content="cafe" />
+        <title>Juice</title>
+        <meta name="description" content="juice" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-
-      </div>
+      <main className={styles.main}>
+        <div style={{ width: "100vw", height: "100vh" }}>
+          <Canvas camera={{ position: [0, 0, 5] }}>
+            <ambientLight intensity={0.5} />
+            <pointLight position={[10, 10, 10]} />
+            <Cube />
+            <OrbitControls />
+          </Canvas>
+        </div>
+      </main>
     </>
   );
 }
