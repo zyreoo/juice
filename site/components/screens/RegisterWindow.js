f5a06ff10f5ad1e0f8a615d8cb369c42b4cd5e0e
Thomas Wu <62056970+TakumiBC@users.noreply.github.com>
2025-01-23 20:19:14 +0800

    Validate Email

--- site/components/screens/RegisterWindow.js
+++ site/components/screens/RegisterWindow.js
@@ -18,7 +18,8 @@ export default function RegisterWindow({ position, isDragging, isActive, handleM
     }, [showTokenUpload]);
 
     const handleSubmit = async () => {
-        if (!email || email === "nevergonna@giveyouup.com") {
+        if (!email || email === "nevergonna@giveyouup.com" || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+)) {
           window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
           return;
         }
