5683022b212f329e6ff960cdddb6aa71c83bf2c9
SerenityUX <thomas@serenidad.app>
2025-01-28 18:23:49 -0800

    new one

--- express-server/src/routes/video.js
+++ express-server/src/routes/video.js
@@ -6,4 +6,9 @@ export const router = express.Router();
 // Disable body parser for this route as we're using formidable
 router.post('/upload', (req, res, next) => {
   uploadVideo(req, res).catch(next);
+});
+
+// Add an additional route at the root level
+router.post('/', (req, res, next) => {
+  uploadVideo(req, res).catch(next);
 }); 
