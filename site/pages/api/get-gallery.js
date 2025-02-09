e07679be06259acb0d2fb11e7ab3a6e929624892
Charmunk <61657908+isaacfonner@users.noreply.github.com>
2025-02-09 02:29:52 +0000

    started work on gallery api

---
+++ site/pages/api/get-gallery.js
@@ -0,0 +1,36 @@
+import Airtable from 'airtable';
+
+const base = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_BASE_ID);
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
+ 
+    const juiceStart = new Date(Date.now() - 1000000 * 60 * 60 * 1000).toISOString();
+
+    const records = await base('gallery')
+    .select({
+      filterByFormula: `IS_AFTER({created_at}, '${juiceStart}')`,
+      sort: [{ field: 'created_at', direction: 'desc' }]
+    })
+    .all();
+
+   
+
+    const games = records.map(record => ({
+      email: record.fields.email,
+      itchurl: record.fields.itchurl,
+      gamename: record.fields.gamename
+    }));
+
+    res.status(200).json(games);
+  } catch (error) {
+    console.error('Error fetching OMG moments:', error);
+    res.status(500).json({ message: 'Error fetching OMG moments' });
+  }
+} 
