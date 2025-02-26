c18fb4ab381f18f9af5aa1f47d7b15c7bf68f42d
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-26 13:44:21 +0100

    omgMomentsTable

--- site/lib/airtable/index.js
+++ site/lib/airtable/index.js
@@ -4,3 +4,4 @@ export { default as base } from './base';
 // tables
 export { signupsTable } from './signups';
 export { juiceStretchesTable } from './juiceStretches';
+export { omgMomentsTable } from './omgMoments';
